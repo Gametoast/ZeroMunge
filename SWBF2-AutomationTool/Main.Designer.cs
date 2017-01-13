b5da55758081b0bda154684279a8ff3ff578f721
marth8880 <marth8880@gmail.com>
2017-01-13 11:52:50 -0800

    Added an output log with basic line-print functionalityl

--- SWBF2-AutomationTool/Main.Designer.cs
+++ SWBF2-AutomationTool/Main.Designer.cs
@@ -28,46 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tbox_TestTextBox = new System.Windows.Forms.TextBox();
-            this.lbl_TestLabel = new System.Windows.Forms.Label();
+            this.components = new System.ComponentModel.Container();
             this.btn_Submit = new System.Windows.Forms.Button();
+            this.clist_Files = new System.Windows.Forms.CheckedListBox();
+            this.btn_AddFiles = new System.Windows.Forms.Button();
+            this.openDlg_AddFilesPrompt = new System.Windows.Forms.OpenFileDialog();
+            this.btn_RemoveFiles = new System.Windows.Forms.Button();
+            this.FormTooltips = new System.Windows.Forms.ToolTip(this.components);
+            this.text_OutputLog = new System.Windows.Forms.RichTextBox();
+            this.lbl_OutputLogLines = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // tbox_TestTextBox
-            // 
-            this.tbox_TestTextBox.Location = new System.Drawing.Point(93, 15);
-            this.tbox_TestTextBox.Name = "tbox_TestTextBox";
-            this.tbox_TestTextBox.Size = new System.Drawing.Size(279, 20);
-            this.tbox_TestTextBox.TabIndex = 1;
-            // 
-            // lbl_TestLabel
-            // 
-            this.lbl_TestLabel.AutoSize = true;
-            this.lbl_TestLabel.Location = new System.Drawing.Point(90, 55);
-            this.lbl_TestLabel.Name = "lbl_TestLabel";
-            this.lbl_TestLabel.Size = new System.Drawing.Size(0, 13);
-            this.lbl_TestLabel.TabIndex = 2;
             // 
             // btn_Submit
             // 
-            this.btn_Submit.Location = new System.Drawing.Point(13, 13);
+            this.btn_Submit.Location = new System.Drawing.Point(670, 527);
             this.btn_Submit.Name = "btn_Submit";
-            this.btn_Submit.Size = new System.Drawing.Size(75, 23);
-            this.btn_Submit.TabIndex = 3;
-            this.btn_Submit.Text = "Submit";
+            this.btn_Submit.Size = new System.Drawing.Size(103, 23);
+            this.btn_Submit.TabIndex = 0;
+            this.btn_Submit.Text = "Run";
+            this.FormTooltips.SetToolTip(this.btn_Submit, "Executes each file in the list recursively.");
             this.btn_Submit.UseVisualStyleBackColor = true;
             this.btn_Submit.Click += new System.EventHandler(this.btn_Submit_Click);
+            // 
+            // clist_Files
+            // 
+            this.clist_Files.FormattingEnabled = true;
+            this.clist_Files.Location = new System.Drawing.Point(13, 13);
+            this.clist_Files.Name = "clist_Files";
+            this.clist_Files.ScrollAlwaysVisible = true;
+            this.clist_Files.Size = new System.Drawing.Size(650, 319);
+            this.clist_Files.TabIndex = 1;
+            // 
+            // btn_AddFiles
+            // 
+            this.btn_AddFiles.AccessibleDescription = "";
+            this.btn_AddFiles.AllowDrop = true;
+            this.btn_AddFiles.Location = new System.Drawing.Point(670, 13);
+            this.btn_AddFiles.Name = "btn_AddFiles";
+            this.btn_AddFiles.Size = new System.Drawing.Size(102, 23);
+            this.btn_AddFiles.TabIndex = 2;
+            this.btn_AddFiles.Tag = "";
+            this.btn_AddFiles.Text = "Add...";
+            this.FormTooltips.SetToolTip(this.btn_AddFiles, "Opens a prompt to add files to the list of files.");
+            this.btn_AddFiles.UseVisualStyleBackColor = true;
+            this.btn_AddFiles.Click += new System.EventHandler(this.btn_AddFiles_Click);
+            // 
+            // openDlg_AddFilesPrompt
+            // 
+            this.openDlg_AddFilesPrompt.Multiselect = true;
+            this.openDlg_AddFilesPrompt.FileOk += new System.ComponentModel.CancelEventHandler(this.openDlg_AddFilesPrompt_FileOk);
+            // 
+            // btn_RemoveFiles
+            // 
+            this.btn_RemoveFiles.Location = new System.Drawing.Point(670, 43);
+            this.btn_RemoveFiles.Name = "btn_RemoveFiles";
+            this.btn_RemoveFiles.Size = new System.Drawing.Size(102, 23);
+            this.btn_RemoveFiles.TabIndex = 3;
+            this.btn_RemoveFiles.Text = "Remove";
+            this.FormTooltips.SetToolTip(this.btn_RemoveFiles, "Removes the selected file.");
+            this.btn_RemoveFiles.UseVisualStyleBackColor = true;
+            this.btn_RemoveFiles.Click += new System.EventHandler(this.btn_RemoveFiles_Click);
+            // 
+            // text_OutputLog
+            // 
+            this.text_OutputLog.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.text_OutputLog.ForeColor = System.Drawing.Color.Black;
+            this.text_OutputLog.Location = new System.Drawing.Point(12, 338);
+            this.text_OutputLog.Name = "text_OutputLog";
+            this.text_OutputLog.ReadOnly = true;
+            this.text_OutputLog.Size = new System.Drawing.Size(651, 212);
+            this.text_OutputLog.TabIndex = 5;
+            this.text_OutputLog.Text = "";
+            this.text_OutputLog.WordWrap = false;
+            // 
+            // lbl_OutputLogLines
+            // 
+            this.lbl_OutputLogLines.AutoSize = true;
+            this.lbl_OutputLogLines.Location = new System.Drawing.Point(670, 338);
+            this.lbl_OutputLogLines.Name = "lbl_OutputLogLines";
+            this.lbl_OutputLogLines.Size = new System.Drawing.Size(44, 13);
+            this.lbl_OutputLogLines.TabIndex = 6;
+            this.lbl_OutputLogLines.Text = "Lines: 1";
             // 
             // AutomationTool
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(384, 261);
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.lbl_OutputLogLines);
+            this.Controls.Add(this.text_OutputLog);
+            this.Controls.Add(this.btn_RemoveFiles);
+            this.Controls.Add(this.btn_AddFiles);
+            this.Controls.Add(this.clist_Files);
             this.Controls.Add(this.btn_Submit);
-            this.Controls.Add(this.lbl_TestLabel);
-            this.Controls.Add(this.tbox_TestTextBox);
             this.MaximizeBox = false;
-            this.MinimumSize = new System.Drawing.Size(400, 300);
+            this.MinimumSize = new System.Drawing.Size(800, 600);
             this.Name = "AutomationTool";
             this.Text = "Automation Tool";
             this.Load += new System.EventHandler(this.AutomationTool_Load);
@@ -77,9 +132,14 @@
         }
 
         #endregion
-        private System.Windows.Forms.TextBox tbox_TestTextBox;
-        private System.Windows.Forms.Label lbl_TestLabel;
         private System.Windows.Forms.Button btn_Submit;
+        private System.Windows.Forms.CheckedListBox clist_Files;
+        private System.Windows.Forms.Button btn_AddFiles;
+        private System.Windows.Forms.OpenFileDialog openDlg_AddFilesPrompt;
+        private System.Windows.Forms.Button btn_RemoveFiles;
+        private System.Windows.Forms.ToolTip FormTooltips;
+        private System.Windows.Forms.RichTextBox text_OutputLog;
+        private System.Windows.Forms.Label lbl_OutputLogLines;
     }
 }
 
--- SWBF2-AutomationTool/Main.cs
+++ SWBF2-AutomationTool/Main.cs
@@ -22,9 +22,67 @@ namespace SWBF2_AutomationTool
 
         }
 
+        private void ExecuteFile(String filePath)
+        {
+            System.Diagnostics.Process.Start(filePath);
+        }
+
+        private void WriteToOutputLog(String message)
+        {
+            if (message != null)
+            {
+                // Print message on new line
+                text_OutputLog.AppendText(message + Environment.NewLine);
+
+                // Auto-scroll to the most recent line
+                text_OutputLog.ScrollToCaret();
+
+                // Is the log full?
+                if (text_OutputLog.TextLength >= (text_OutputLog.MaxLength - 500))
+                {
+                    //lbl_OutputLogLines.Text = ("Lines: " + text_OutputLog.Lines.Count().ToString());
+                }
+
+                // Update line count
+                lbl_OutputLogLines.Text = ("Lines: " + text_OutputLog.Lines.Count().ToString());
+
+                // TODO: add functionality to remove lines from the beginning when the text box becomes full
+            }
+        }
+
+        private void btn_AddFiles_Click(object sender, EventArgs e)
+        {
+            // Open the 'Add Files' prompt
+            openDlg_AddFilesPrompt.ShowDialog();
+        }
+
+        private void btn_RemoveFiles_Click(object sender, EventArgs e)
+        {
+            // Remove the currently selected file from the list
+            clist_Files.Items.RemoveAt(clist_Files.SelectedIndex);
+        }
+
+        private void openDlg_AddFilesPrompt_FileOk(object sender, CancelEventArgs e)
+        {
+            // Add the selected files to the list
+            foreach (String file in openDlg_AddFilesPrompt.FileNames)
+            {
+                clist_Files.Items.Add(file, true);
+            }
+        }
+
         private void btn_Submit_Click(object sender, EventArgs e)
         {
-            lbl_TestLabel.Text = tbox_TestTextBox.Text;
+            WriteToOutputLog("test");
+
+            // Execute each checked file in the list
+            /*foreach (int curFile in clist_Files.Items)
+            {
+                if (clist_Files.GetItemChecked(curFile) == true)
+                {
+                    System.Diagnostics.Debug.Print(clist_Files.Items[curFile].ToString());
+                }
+            }*/
         }
     }
 }
