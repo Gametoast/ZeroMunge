b451645947e73cbcdc90e567cd566144357a20d3
marth8880 <marth8880@gmail.com>
2017-01-14 00:27:30 -0800

    Added functionality to output to log in real-time

--- SWBF2-AutomationTool/Main.cs
+++ SWBF2-AutomationTool/Main.cs
@@ -6,7 +6,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace SWBF2_AutomationTool
@@ -23,72 +23,6 @@ namespace SWBF2_AutomationTool
 
         }
 
-        // TODO: prevent application from locking up during batch execution
-        /// <summary>
-        /// Executes the specified file.
-        /// </summary>
-        /// <param name="filePath">Full path of the file to execute.</param>
-        private void ExecuteFile(String filePath)
-        {
-            //System.Diagnostics.Process.Start(filePath);
-
-            // Get the file's directory
-            string filePathDir = "";
-            int index = filePath.LastIndexOf(@"\");
-            if (index > 0)
-            {
-                filePathDir = filePath.Substring(0, index); // or index + 1 to keep slash
-            }
-
-            Process proc = new Process();
-            proc.StartInfo.FileName = filePath;
-            proc.StartInfo.WorkingDirectory = filePathDir;
-            proc.StartInfo.CreateNoWindow = true;
-            proc.StartInfo.UseShellExecute = false;
-            proc.StartInfo.RedirectStandardOutput = true;
-            proc.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
-            proc.Start();
-
-            // Log the output
-            string output = proc.StandardOutput.ReadToEnd();
-            WriteToOutputLog(output);
-            proc.WaitForExit();
-        }
-
-        /// <summary>
-        /// Prints the specified text to the output log.
-        /// </summary>
-        /// <param name="message">Text to print.</param>
-        /// <param name="newLine">Optional: True to append a new line to the end of the message.</param>
-        private void WriteToOutputLog(String message, bool newLine = false)
-        {
-            if (message != null)
-            {
-                // Print message
-                text_OutputLog.AppendText(message);
-
-                // Are we supposed to print a new line?
-                if (newLine)
-                {
-                    // Print message on new line
-                    text_OutputLog.AppendText(Environment.NewLine);
-                }
-
-                // Auto-scroll to the most recent line
-                text_OutputLog.ScrollToCaret();
-
-                // Is the log full?
-                if (text_OutputLog.TextLength >= (text_OutputLog.MaxLength - 500))
-                {
-                    //lbl_OutputLogLines.Text = ("Lines: " + text_OutputLog.Lines.Count().ToString());
-                }
-
-                // Update line count
-                lbl_OutputLogLines.Text = ("Lines: " + text_OutputLog.Lines.Count().ToString());
-
-                // TODO: add functionality to remove lines from the beginning when the text box becomes full
-            }
-        }
 
         // When user clicks the 'Add...' button
         private void btn_AddFiles_Click(object sender, EventArgs e)
@@ -97,12 +31,14 @@ namespace SWBF2_AutomationTool
             openDlg_AddFilesPrompt.ShowDialog();
         }
 
+
         // When user clicks the 'Remove' button
         private void btn_RemoveFiles_Click(object sender, EventArgs e)
         {
             // Remove the currently selected file from the list
             clist_Files.Items.RemoveAt(clist_Files.SelectedIndex);
         }
+
 
         // When user clicks the 'OK' button in the 'Add Files...' prompt
         private void openDlg_AddFilesPrompt_FileOk(object sender, CancelEventArgs e)
@@ -114,6 +50,7 @@ namespace SWBF2_AutomationTool
             }
         }
 
+
         // When user clicks the 'Run' button
         private void btn_Submit_Click(object sender, EventArgs e)
         {
@@ -124,6 +61,100 @@ namespace SWBF2_AutomationTool
                 if (clist_Files.GetItemChecked(curFile) == true)
                 {
                     ExecuteFile(clist_Files.GetItemText(clist_Files.Items[curFile]));
+                }
+            }
+        }
+
+        // TODO: add functionality to execute each file one after the other as they finish like a playlist
+        /// <summary>
+        /// Executes the specified file.
+        /// </summary>
+        /// <param name="filePath">Full path of the file to execute.</param>
+        private void ExecuteFile(string filePath)
+        {
+            // Get the file's directory
+            string filePathDir = "";
+            int index = filePath.LastIndexOf(@"\");
+            if (index > 0)
+            {
+                filePathDir = filePath.Substring(0, index); // or index + 1 to keep slash
+            }
+
+            ProcessStartInfo startInfo = new ProcessStartInfo(@filePath)
+            {
+                WorkingDirectory = filePathDir,
+                WindowStyle = ProcessWindowStyle.Hidden,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true
+            };
+            
+            Process proc = new Process();
+            proc.StartInfo = startInfo;
+            proc.EnableRaisingEvents = true;
+            proc.OutputDataReceived += ((sender, e) =>
+            {
+                Thread outputThread = new Thread(() => LogOutputProc(e.Data, true));
+                outputThread.Start();
+            });
+
+            proc.Start();
+            proc.BeginOutputReadLine();
+            //proc.WaitForExit();
+            //Thread.Sleep(5000);
+        }
+
+        // This method is executed on the worker thread and makes a thread-safe call on the RichTextBox control.
+        private void LogOutputProc(string message, bool newLine = false)
+        {
+            LogOutput(message, newLine);
+        }
+
+        // This delegate enables asynchronous calls for setting the text property on a RichTextBox control.
+        delegate void LogOutputCallback(string message, bool newLine = false);
+        
+        /// <summary>
+        /// Prints the specified text to the output log.
+        /// </summary>
+        /// <param name="message">Text to print.</param>
+        /// <param name="newLine">Optional: True to append a new line to the end of the message.</param>
+        private void LogOutput(string message, bool newLine = false)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (text_OutputLog.InvokeRequired)
+            {
+                LogOutputCallback cb = new LogOutputCallback(LogOutput);
+                BeginInvoke(cb, new object[] { message, newLine });
+            }
+            else
+            {
+                if (message != null)
+                {
+                    // Print message
+                    text_OutputLog.AppendText(message);
+
+                    // Are we supposed to print a new line?
+                    if (newLine)
+                    {
+                        // Print message on new line
+                        text_OutputLog.AppendText(Environment.NewLine);
+                    }
+
+                    // Auto-scroll to the most recent line
+                    text_OutputLog.ScrollToCaret();
+
+                    // Is the log full?
+                    if (text_OutputLog.TextLength >= (text_OutputLog.MaxLength - 500))
+                    {
+                        //lbl_OutputLogLines.Text = ("Lines: " + text_OutputLog.Lines.Count().ToString());
+                    }
+
+                    // Update line count
+                    lbl_OutputLogLines.Text = ("Lines: " + text_OutputLog.Lines.Count().ToString());
+
+                    // TODO: add functionality to remove lines from the beginning when the text box becomes full
                 }
             }
         }
