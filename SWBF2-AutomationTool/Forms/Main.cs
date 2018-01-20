b2d9b12014b4037ae33af07231091816b5a00ce4
marth8880 <marth8880@gmail.com>
2018-01-20 01:14:31 -0800

    Revert "- Add thread-safe starting and stopping of listener server;"
    This reverts commit 14cf6218949a4cbc6e0bb75b4e56cde310f3d9d7.

--- SWBF2-AutomationTool/Forms/Main.cs
+++ SWBF2-AutomationTool/Forms/Main.cs
@@ -137,17 +137,17 @@ namespace AutomationTool
 				}
 			}
 
-			
+			SetUpdateStatusBar(updateAvailable);
 		}
-		
 
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="available"></param>
 		public void SetUpdateStatusBar(bool available)
 		{
-			this.SetUpdateStatusBar(available, null);
-		}
-		public void SetUpdateStatusBar(bool available, string downloadUrl)
-		{
-			// do stuff
+
 		}
 
 
@@ -164,10 +164,6 @@ namespace AutomationTool
 		}
 
 
-		/// <summary>
-		/// If there is an internet connection, checks for updates and returns true if an update is available.
-		/// </summary>
-		/// <returns>True if an update is available, false if not.</returns>
 		public static bool CheckForUpdates()
 		{
 			Trace.WriteLine("Checking for application updates...");
@@ -199,9 +195,6 @@ namespace AutomationTool
 		}
 
 
-		/// <summary>
-		/// Starts the update flow (duh).
-		/// </summary>
 		public static void StartUpdateFlow()
 		{
 			OpenWindow_Updates();
